7a5f6faf83efe1e464883c77f521580c63abe84f
alpha597 <142709319+alpha597@users.noreply.github.com>
2024-03-24 10:14:33 +0530

    Update SquarePanel.java

--- java snake game/src/SquarePanel.java
+++ java snake game/src/SquarePanel.java
@@ -1,4 +1,14 @@
+import java.awt.Color;
+import javax.swing.JPanel;
+public class SquarePanel  extends JPanel{
+    private static final long serialUid = 1L;
 
-public class SquarePanel {
+       public SquarePanel(Color c){
+        this.setBackground(c);
+       }
+       public void ChangeColor(Color d){
+        this.setBackground(d);
+        this.repaint();
+       }
 
 }
